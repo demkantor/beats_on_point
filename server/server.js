e1b6b74de37e63cd2510370f750b0107253ce3a5
demkantor <demkantor@gmail.com>
2020-03-17 21:55:20 -0500

    initial commit after setup

---
+++ server/server.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+require('dotenv').config();
+const { Client } = require('pg');
+const app = express();
+const bodyParser = require('body-parser');
+const sessionMiddleware = require('./modules/session-middleware');
+
+const passport = require('./strategies/user.strategy');
+
+// Route includes
+const userRouter = require('./routes/user.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Routes */
+app.use('/api/user', userRouter);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+//* say hi to postgres *//
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'con',
+  password: 'secretpass',
+})
+client
+  .connect()
+  .then(() => console.log('postgres database connected....'))
+  .catch(err => console.error('connection error', err.stack))
